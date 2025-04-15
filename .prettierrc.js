ee3ce25eb29d25d8ff1a747277aefb5f9a53d6e0
Mario Lopez <modulor@hotmail.es>
2025-04-15 14:43:29 -0600

    fix prettier

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+export default {
+  plugins: ["prettier-plugin-astro"],
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  printWidth: 140,
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
